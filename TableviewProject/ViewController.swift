1d0060fbc331cd043864113fd906327a6f4fc2a0
Can Yoldas <can.yoldas0@outlook.com>
2021-10-09 20:01:23 +0300

    Initial Commit

---
+++ TableviewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableviewProject
+//
+//  Created by Can Yoldas on 9.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
